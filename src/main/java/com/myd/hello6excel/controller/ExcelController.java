b54041b173b309f93042c7decc8b224ed480cc6b
OF3787 <mayuanding@qianmi.com>
2021-03-12 17:24:52 +0800

    controller

---
+++ src/main/java/com/myd/hello6excel/controller/ExcelController.java
@@ -0,0 +1,10 @@
+package com.myd.hello6excel.controller;
+
+/**
+ * @author <a href="mailto:mayuanding@qianmi.com">OF3787-马元丁</a>
+ * @version 0.1.0
+ * @Date:2021/3/12 17:24
+ * @Description:
+ */
+public class ExcelController {
+}
