e3278509c50ce06739fdfca5ef891b76b79de848
OF3787 <mayuanding@qianmi.com>
2021-03-12 17:25:27 +0800

    controller

--- src/main/java/com/myd/hello6excel/controller/ExcelController.java
+++ src/main/java/com/myd/hello6excel/controller/ExcelController.java
@@ -1,10 +1,13 @@
 package com.myd.hello6excel.controller;
 
+import org.springframework.stereotype.Controller;
+
 /**
  * @author <a href="mailto:mayuanding@qianmi.com">OF3787-马元丁</a>
  * @version 0.1.0
  * @Date:2021/3/12 17:24
  * @Description:
  */
+@Controller
 public class ExcelController {
 }
